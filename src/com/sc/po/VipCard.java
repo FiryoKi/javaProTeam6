f65dc5df7cf056b45e8e4bad5559efeca49a4069
linranran1 <1440985153@qq.com>
2023-03-17 11:57:23 +0800

    1

---
+++ src/com/sc/po/VipCard.java
@@ -0,0 +1,54 @@
+package com.sc.po;
+
+public class VipCard {
+	private int cid;
+	private String name;
+	private String password;
+	private String card;
+	private int point;
+	private String cdate;
+	private int pid;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getCard() {
+		return card;
+	}
+	public void setCard(String card) {
+		this.card = card;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	public String getCdate() {
+		return cdate;
+	}
+	public void setCdate(String cdate) {
+		this.cdate = cdate;
+	}
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+}
