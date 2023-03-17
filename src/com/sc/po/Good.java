243093bd9869d04646595b2d259eec4a395e82c7
1 <1687859100@qq.com>
2023-03-17 10:18:58 +0800

    add message..

---
+++ src/com/sc/po/Good.java
@@ -0,0 +1,32 @@
+package com.sc.po;
+
+public class Good {
+    private int Gid;
+    private String GoodName;
+    private int NeedPoint;
+    private int Number;
+    public int getGid() {
+        return Gid;
+    }
+    public void setGid(int gid) {
+        Gid = gid;
+    }
+    public String getGoodName() {
+        return GoodName;
+    }
+    public void setGoodName(String goodName) {
+        GoodName = goodName;
+    }
+    public int getNeedPoint() {
+        return NeedPoint;
+    }
+    public void setNeedPoint(int needPoint) {
+        NeedPoint = needPoint;
+    }
+    public int getNumber() {
+        return Number;
+    }
+    public void setNumber(int number) {
+        Number = number;
+    }
+}
