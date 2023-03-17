09fc32e1d8775a0fe055471944d692e24030a582
linranran1 <1440985153@qq.com>
2023-03-17 15:26:51 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/com/sc/dao/VipCardDao.java

--- src/com/sc/view/MarketView.java
+++ src/com/sc/view/MarketView.java
@@ -1,7 +1,9 @@
 package com.sc.view;
 
 import com.sc.po.Good;
+import com.sc.po.VipCard;
 import com.sc.service.GoodService;
+import com.sc.service.VipCardService;
 import com.sc.util.InputUtil;
 
 import java.util.List;
@@ -11,7 +13,7 @@ public class MarketView {
 
 
     public static void main(String[] args) {
-
+       Viplogin();
         System.out.println("请选择：1.查询并兑换可兑换物品\t2.新增可兑换商品\t3.删除可兑换商品\t4.修改可兑换商品\t5.返回主页面");
         System.out.println("请输入你要操作的选项：");
         int i=InputUtil.getInt();
@@ -43,7 +45,64 @@ public class MarketView {
 
         }
     }
-static GoodService gs=new GoodService();
+    static VipCardService vs=new VipCardService();
+    //会员修改密码
+    public static void updatePassword(){
+
+
+
+        System.out.println("请输入需要修改的用户密码：");
+        String password=InputUtil.getString();
+        //将获取到的值存储到对象中
+        VipCard v=new VipCard();
+
+        v.setPassword(password);
+        boolean flag=vs.updatePassword(v);
+        if(flag){
+            System.out.println("修改成功！");
+        }else {
+            System.out.println("修改失败！");
+        }
+    }
+    //会员登录
+    public static void Viplogin() {
+        int count=1;
+        while(count<4){
+            System.out.println("欢迎");
+            System.out.println("用户名：");
+            String name=InputUtil.getString();
+
+            System.out.println("密码：");
+            String password=InputUtil.getString();
+            VipCard v=new VipCard();
+            v.setName(name);
+            v.setPassword(password);
+            VipCard vip=vs.login(v);
+
+            if(vip!=null&&name.equals(vip.getName())&&password.equals(vip.getPassword())){
+                System.out.println("恭喜"+vip.getName()+"登录成功！");
+                System.out.println("请选择：1.兑换商品\t2.修改密码\t3.查询剩余积分\t0.退出");
+                int n=InputUtil.getInt();
+                switch(n) {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        updatePassword();
+                        break;
+                    case 3:
+                        break;
+                    case 0:
+                        break;
+                }
+            }	else{
+                System.out.println("登录失败，请重新登录。");
+                count++;
+            }
+        }
+    }
+    static GoodService gs=new GoodService();
 //查询所有
 public static void selectAll() {
         List<Good> good=gs.selectAll();
