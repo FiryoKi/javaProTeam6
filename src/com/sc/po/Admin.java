75eee625db41bbcd16bb2083a4c2ec3d176b23c1
FiryoKi <1491035037@qq.com>
2023-03-17 14:40:02 +0800

    管理员po

--- src/com/sc/po/Admin.java
+++ src/com/sc/po/Admin.java
@@ -1,6 +1,40 @@
 package com.sc.po;
 
 public class Admin {
+    int Uid;
+    String Admin;
+    String Password;
+    int PID = 1;
 
+    public int getUid() {
+        return Uid;
+    }
 
+    public void setUid(int uid) {
+        Uid = uid;
+    }
+
+    public String getAdmin() {
+        return Admin;
+    }
+
+    public void setAdmin(String admin) {
+        Admin = admin;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public int getPID() {
+        return PID;
+    }
+
+    public void setPID(int PID) {
+        this.PID = PID;
+    }
 }
