a0134e8d247acabe2cf0be44138b5996bd89e224
FiryoKi <1491035037@qq.com>
2023-03-18 14:38:56 +0800

    界面

--- src/com/sc/view/AdminView.java
+++ src/com/sc/view/AdminView.java
@@ -4,6 +4,8 @@ package com.sc.view;
 import com.sc.service.AdminService;
 import com.sc.util.InputUtil;
 
+
+
 /**
  * 管理员界面
  */
@@ -13,12 +15,12 @@ public class AdminView {
     public void adminView() {
         System.out.println("管理界面");
         System.out.println("请选择：" +
-                "1.注册开卡" +
-                "2.修改密码" +
-                "3.积分累计" +
-                "4.查询剩余积分" +
-                "5.查看所有会员信息" +
-                "6.管理积分兑换商品" +
+                "1.注册开卡\t" +
+                "2.修改密码\t" +
+                "3.积分累计\t" +
+                "4.查询剩余积分\t" +
+                "5.查看所有会员信息\t" +
+                "6.管理积分兑换商品\t" +
                 "7.退出");
         System.out.println("请输入你要操作的选项：");
         int i = InputUtil.getInt();
@@ -41,7 +43,7 @@ public class AdminView {
 
                 break;
             case 7:
-
+                System.exit(0);
                 break;
             default:
                 System.out.println("没有该选项，请重新选择！！");
